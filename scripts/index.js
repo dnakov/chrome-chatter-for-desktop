5a67cc1888926b944aa0746b3175de391c468f64
dnakov <daniel.nakov@gmail.com>
2014-05-30 18:34:47 -0400

    background

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,17 @@ var bg, sf1;
 
 function setPageLocation() {
     bg.lastUrl = sf1.src;
+    chrome.storage.sync.set({'url': bg.lastUrl});
 }
 
 window.onload = function() {
+    chrome.storage.sync.get(function(data) {
+        sf1 = document.getElementById('sf1');
+        sf1.src = data.url || "https://login.salesforce.com/?ec=302&startURL=%2Fone%2Fone.app";
+    });
     chrome.runtime.getBackgroundPage(function(win) {
         bg = win;
-        sf1 = document.getElementById('sf1');
-        sf1.src = bg.lastUrl || "https://login.salesforce.com/?ec=302&startURL=%2Fone%2Fone.app";
+        setInterval(setPageLocation, 1000);
     });
-    setInterval(setPageLocation, 1000);
 }
 
